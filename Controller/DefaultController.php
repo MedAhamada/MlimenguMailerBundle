0233eb66e89e062eb0cf5522b039f1ffc12138ee
hebermit <mit2015>
2015-12-08 14:11:41 +0000

    First Commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Mlimengu\MailerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('MlimenguMailerBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ MlimenguMailerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Mlimengu\MailerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MlimenguMailerBundle extends Bundle
+{
+}
